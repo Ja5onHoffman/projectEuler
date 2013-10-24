816693fe8f40c26ef21c065aedb6beb6776e9252
Ja5onHoffman <jasonandrewhoffman@gmail.com>
2013-10-24 19:05:53 -0400

    Change calculates only largest palindromes but returns correct answer

--- pEuler4.js
+++ pEuler4.js
@@ -1,19 +1,19 @@
 var palindromic = function() {
-	var a = [];
-	var res = [];
-	for (var i = 100; i < 999; i++) {
-		a.push(i);
-	}
-	var ar = a.slice(0);
-	a.map(function(x) {
-		for (var j = 0; j < ar.length; j++) {
-			var result = x * ar[j];
-			if (result.toString() === result.toString().split("").reverse().join("")) {
-				res.push(result);
-			}
-		}
-	})
-	var answer = res.sort().pop();
-	res.sort();
-	return answer;
-};
+var a = [];
+var res = [];
+for (var i = 900; i < 1000; i++) {
+    a.push(i);
+}
+var largestNumber = 0;
+var ar = a.slice(0);
+a.map(function(x) {
+    for (var j = 0; j < ar.length; j++) {
+        var result = x * ar[j];
+        if (result.toString() ===   result.toString().split("").reverse().join("")) {
+            res.push(result);
+        }
+    }
+  });
+  var answer = res.sort().pop();
+  return answer;
+ };
